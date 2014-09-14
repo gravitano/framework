17ad94b61ce6e5c7503e4753223026f5df9597b6
gravitano <gue.warsono@gmail.com>
2014-09-14 16:11:43 +0700

    Clean up

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -1,16 +1,13 @@
 <?php
 
 return [
-
-	'public'	=>	__DIR__ . '/../public',
-    'resources'	=>	__DIR__ . '/../resources',
-    'src'		=>	__DIR__ . '/../src',
-    'app'	    =>	__DIR__ . '/../src/App',
-    'tests'		=>	__DIR__ . '/../tests',
-	'config'	=>	__DIR__ . '/../config',
-
-	'controller'	=>	__DIR__ . '/../src/Http/Controllers',
-	'model'			=>	__DIR__ . '/../src/Database/Models',
-	'view'			=>	__DIR__ . '/../resources/views',
-
+    'public' => __DIR__ . '/../public',
+    'resources' => __DIR__ . '/../resources',
+    'src' => __DIR__ . '/../src',
+    'app' => __DIR__ . '/../src/App',
+    'tests' => __DIR__ . '/../tests',
+    'config' => __DIR__ . '/../config',
+    'controller' => __DIR__ . '/../src/Http/Controllers',
+    'model' => __DIR__ . '/../src/Database/Models',
+    'view' => __DIR__ . '/../resources/views',
 ];
